08a5d91a54735f05309ac7e97d16419752cb8995
Vandy TRIVEDI <270026288@bun2-204-19.local>
2019-12-02 18:01:20 +1300

    MondayEvening

--- js/script.js
+++ js/script.js
@@ -14,8 +14,10 @@ var transport = [
       minPpl: 1,
       maxPpl: 1,
       rent   : 109,
+      rating : 'images/star.png',
       fuel   : 'Petrol',
       mileage   : "3.7L/100km",
+      test    : 3.7,
       engineCC   : "800cc",
       photo: "images/bikes/bike1.jpg"
   },
@@ -25,15 +27,17 @@ var transport = [
       vehicleName : "TOYOTA YARIS SX 1.5P/4AT/HA/5DR/5",
       transmission: 'Automatic',
       year: 2015,
-      description: 'The Yaris sets the bar high in looks, performance safety and features. The Yaris offers more than you would expect. The Yaris boasts a sportier look along with a modern and spacious interior and alloy wheels. Sports flair, family first, the Toyota Yaris Hatch adds an enviable sports flair, whilst safely bringing the kids along for the ride.Yaris Hatch is bursting with funky features and cool technologies. Great looking and with a bubbly personality to match, it’s not only a fun way to get around, but safe and stylish too. Everyone needs a little Yaris in their life. Yaris has proven itself a dependable hatchback that offers so much more.',
-      features: "New Zealand new, Iso Fixings, 7 Airbags, 15 inch Alloy Wheels, Fog Lights, ABS, Power Steering, Cruise Control, Traction Control, Electronic Stability Control, Automatic Headlights, Parking Sensors, Reversing Camera, Air Conditioning, CD Player, Bluetooth, USB/AUX/MP3 Input, Electric Mirrors, Electric Windows, Multi-Function Steering Wheel, Central Locking.",
+      description: 'The Yaris sets the bar high in looks, performance safety and features. The Yaris offers more than you would expect. The Yaris boasts a sportier look along with a modern and spacious interior and alloy wheels. Sports flair, family first.',
+      features: "New Zealand new, 7 Airbags, 15 inch Alloy Wheels, Fog Lights,Cruise Control, Traction Control, Electronic Stability Control,Parking Sensors, Reversing Camera, Air Conditioning, CD Player, Bluetooth, USB/AUX/MP3 Input,Central Locking.",
       minDay: 1,
       maxDay: 10,
       minPpl: 1,
       maxPpl: 2,
       rent   : 129,
+      rating : "images/star.png",
       fuel   : 'Petrol',
-      mileage   : "8.5L/100km",
+      mileage : "8.5L/100km",
+      test    : 8.5,
       engineCC   : "1497cc",
       photo: "images/small/small1.jpg"
   },
@@ -43,15 +47,17 @@ var transport = [
       vehicleName : "MAZDA CX8 LIMITED AWD",
       transmission: "Automatic",
       year: 2015,
-      description: "Imagination is the world’s most valuable resource. It’s our window to the future, because if we can imagine it, we can create it. Mazda is built on a foundation of imagination and ingenuity. It’s the fuel behind all we create and achieve. It has given us SKYACTIV TECHNOLOGY, ‘KODO — Soul of Motion’ design and countless successes that once existed only in someone’s imagination. These pages show how imagination is an integral part of our innovative cars. First-Ever Mazda CX-8 Diesel brings a level of sleek design and diesel performance and efficiency that you wouldn’t expect in a seven-seat SUV. Featuring all the luxury and technology Mazda SUVs have become known for, as well as the latest in advanced safety technology, you’ll discover an outstanding SUV experience for growing families. Mazda’s stunning KODO Design language is at the heart of every Mazda SUV. First-Ever Mazda CX-8 Diesel is the latest example, offering the spacious versatility of a large SUV but with smaller dimensions that don’t compromise on interior space or cabin comfort.",
-      features: "7 seats, Air conditioning Automatic, 3-zone Climate Control with Rear Vents, Bluetooth® handsfree phone connectivity, MZD Connect Infotainment System, Cabin Air Filter Ventilation pollen filter, Cruise Control, Seat adjustment - front Manual 6-way (Driver), Manual 6-way (Passenger), Steering column - adjustable, Apple Carplay™ and Android Auto™ Optional Accessory, Auxiliary input 3.5mm MP3 player compatible, 4 x USB input, iPod® connectivity, Bluetooth® audio, Internet Radio Integration Yes (Stitcher™; Aha™ with Facebook™ and Twitter™), Speakers 6, Steering-wheel-mounted audio controls, USB input, Cup holders, Interior illumination, Black cabin headlining, Leather wrapped shift knob, Leather wrapped steering wheel, MZD Connect Infotainment System (7 inch colour touchscreen display, commander dial, voice control, steering wheel controls), Push button engine start, Rear seats - split-fold, Satellite navigationSeat adjustment - front, Rear spoiler, dual chrome exhaust extensions, roof rails, Fog-lampsLED, High Beam Control (HBC), automatic on/off function, coming/leaving home lighting, Keyless entry - remote central locking, Mirrors with Body-coloured, power adjustable, side indicator, automatic power fold, Windscreen wipers - front 2-speed with rain-sensing function, Active Bonnet.",
+      description: "Mazda’s stunning KODO Design language is at the heart of every Mazda SUV. First-Ever Mazda CX-8 Diesel is the latest example, offering the spacious versatility of a large SUV but with smaller dimensions that don’t compromise on interior space or cabin comfort.",
+      features: "7 seats, Air conditioning Automatic, 3-zone Climate Control with Rear Vents, Bluetooth® handsfree phone connectivity, MZD Connect Infotainment System, Cabin Air Filter Ventilation pollen filter, Cruise Control, Seat adjustment - front Manual 6-way (Driver), Manual 6-way (Passenger), Steering column - adjustable, Apple Carplay™ and Android Auto™ Optional Accessory, Auxiliary input 3.5mm MP3 player compatible, 4 x USB input, iPod® connectivity, Bluetooth® audio.",
       minDay: 3,
       maxDay: 10,
       minPpl: 1,
       maxPpl: 5,
       rent   : 144,
+      rating : "images/star.png",
       fuel   : 'Diesel',
       mileage   : "9.7L/100km",
+      test    : 9.7,
       engineCC   : "2191cc",
       photo: "images/large/large1.jpg"
   },
@@ -61,15 +67,17 @@ var transport = [
     vehicleName : "FORD TRANSIT 470E JUMBO",
     transmission: "Manual",
     year: 2019,
-    description: "Ford Transit Commercial Vehicles are known for their reliability, loadspace, comfort and efficiency. With a next generation diesel engine available with 6-speed automatic transmission.Tested for Toughness. Ford ran vehicles at maximum speed for two months. Took them from -40°C to towing fully-laden trailers in 50°C heat. We opened and closed the doors and bonnet over 250,000 times. So if you need tough and reliable, the Transit Commercial Vehicle range has got what it takes. Makes work a pleasure. Whether Transit Custom or Cargo, there’s a range of intelligent features to help lighten the load. Car-like comfort makes work a pleasure, while high-quality materials give a premium feel without compromising everyday practicality.",
-    features: "16-inch steel wheels, Roll over mitigation, Torque Vectoring Control, Drive away locking, Power assisted steerting, Rear mudflaps, Drivers armrest, Tachomater, Trip computer, Compartment lamps, 3.5-inch Multi-functional display screen.",
+    description: "Ford Transit Commercial Vehicles are known for their reliability, loadspace, comfort and efficiency. With a next generation diesel engine available with 6-speed automatic transmission.Tested for Toughness. Ford ran vehicles at maximum speed for two months. Took them from -40°C to towing fully-laden trailers in 50°C heat.",
+    features: "16-inch steel wheels, Roll over mitigation, Torque Vectoring Control, Drive away locking, Power assisted steerting,Drivers armrest, Tachomater, Trip computer, Compartment lamps, 3.5-inch Multi-functional display screen.",
     minDay: 2,
     maxDay: 15,
     minPpl: 2,
     maxPpl: 6,
     rent   : 200,
+    rating : "images/star.png",
     fuel   : 'Diesel',
     mileage   : "17L/100km",
+    test    : 17,
     engineCC   : "1995cc",
     photo: "images/mh/mh1.jpg"
   },
@@ -85,11 +93,20 @@ script.src = 'https://maps.googleapis.com/maps/api/js?key=' + myKey[0].key + '&l
 document.getElementsByTagName('body')[0].appendChild(script);
 //map stylesheet
 
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script
+// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+var distance;
 function initMap() {
+  //console.log('test');
+  // document.getElementById('map').style.display = "none";
+
   var map = new google.maps.Map(document.getElementById('map'), {
     mapTypeControl: false,
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13
+    center: {lat: -40.9006, lng: 174.8860},
+    zoom: 6
   });
 
   new AutocompleteDirectionsHandler(map);
@@ -137,7 +154,7 @@ function AutocompleteDirectionsHandler(map) {
 // Autocomplete.
 AutocompleteDirectionsHandler.prototype.setupClickListener = function(
     id, mode) {
-  var radioButton = document.getElementById(id);
+  // var radioButton = document.getElementById(id);
   var me = this;
 
   // radioButton.addEventListener('click', function() {
@@ -180,12 +197,17 @@ AutocompleteDirectionsHandler.prototype.route = function() {
         travelMode: this.travelMode
       },
       function(response, status) {
+        console.log(response);
+        //console.log(response.routes[0].legs[0].distance.text);
+        distance = response.routes[0].legs[0].distance.text;
+
         if (status === 'OK') {
           me.directionsRenderer.setDirections(response);
         } else {
           window.alert('Directions request failed due to ' + status);
         }
       });
+      return (distance);
 };    //End of address autofield
 
 //Home Menu
@@ -239,43 +261,60 @@ function allTransport(){
   document.getElementById('show').innerHTML = " "; //to clear the container
 for(var i = 0; i < transport.length; i++) {
   document.getElementById('show').innerHTML
-  += '<div class="vCard border border-dark py-2 mt-3 mx-1 ml-5 myContainer clearfix col-sm-6 col-md-6 col-lg-4">'
+  += '<div class="vCard py-2 mt-3 mx-1 ml-5 myContainer clearfix col-sm-6 col-md-6 col-lg-4">'
 
   +  '<img src="' + transport[i].photo + ' " class="card-img-top" alt="Vehicles"/>'
   +  '<ul class="py-0 px-0 list-group list-group-flush">'
   + '<li class="text-primary cardContent list-group-item">' + transport[i].vehicleName + '</li>'
   + '<li class="text-primary cardContent list-group-item" >' + transport[i].type + '</li>'
   + '<li class="text-primary cardContent list-group-item" > Rent: $ ' + transport[i].rent + ' per day </li>'
-  + '<button id= " ' + transport[i].id + ' " type="button" class="btn btn-primary modalClass" data-toggle="modal" data-target=".bd-example-modal-lg">More</button>'
+  + '<img src="' + transport[i].rating + ' " class="card-img-top" alt="Vehicles"/>'
+  + '<br><button id= " ' + transport[i].id + ' " type="button" class="btn btn-primary modalClass" data-toggle="modal" data-target=".bd-example-modal-lg">More</button>'
   +  '</ul>'
   +  '</div>'
-
-  // += '<div class="row ml-4">'
-  // +'<div class="col">'
-  // + '<img class="img-thumbnail mt-3" src="' + transport[i].photo + ' "  alt="Vehicles"/>'
-  // + '</div>'
-  // + '<div class="col">'
-  // + '</br><h5 class="text-danger">' + transport[i].vehicleName + '</h5>'
-  // + '</br>Year : <h5 class="text-danger" >' + transport[i].year + '</h5>'
-  // + '</br><h5 class="text-danger" > ' + transport[i].rent + '</h5>'
-  // + '</div>'
-  // + '</div>';
+//
+//   // += '<div class="row ml-4">'
+//   // +'<div class="col">'
+//   // + '<img class="img-thumbnail mt-3" src="' + transport[i].photo + ' "  alt="Vehicles"/>'
+//   // + '</div>'
+//   // + '<div class="col">'
+//   // + '</br><h5 class="text-danger">' + transport[i].vehicleName + '</h5>'
+//   // + '</br>Year : <h5 class="text-danger" >' + transport[i].year + '</h5>'
+//   // + '</br><h5 class="text-danger" > ' + transport[i].rent + '</h5>'
+//   // + '</div>'
+//   // + '</div>';
   }
 };
 
 allTransport();
-
+// initMap();
+modals();
 //modals
-
+function modals(){
 $('.modalClass').on('click', function(){
-  console.log(this.id);
-  console.log(typeof(this.id));
+  $('.myModal').show();
+  //console.log(this.id);
+  //console.log(typeof(this.id));
+  console.log(days);
+  console.log(people);
+  console.log(distance);
+  document.getElementById('modalContent').innerHTML = " ";
+
+  // distance = 675;
+ distance = parseInt(distance.substring(0,3));
+ console.log
+  // var res = str.substring(0, 2);
+  console.log(distance);
+
   for(var i = 0; i < transport.length; i++) {
   if (parseInt(this.id) === parseInt(transport[i].id)) {
+
+    var cost = distance/100 * transport[i].rent*2.66;
+    console.log(cost);
+
     console.log('ID matched');
-    document.getElementById('modalContent').innerHTML = ""
-    document.getElementById('modalContent').innerHTML
-    += '<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">'
+    document.getElementById('modalContent').innerHTML +=
+      '<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">'
     +  '<div class="carousel-inner">'
     +  '<div class="carousel-item active">'
     +  '<img src=" ' + transport[i].photo + ' " class="d-block w-50 center" alt="..."></div><div class="carousel-item">'
@@ -299,22 +338,23 @@ $('.modalClass').on('click', function(){
     + '<h6 class="w-100"><b> Mileage: </b>' + transport[i].mileage + '</h6>'
     + '<h6 class="w-100"><b> Engine Capacity: </b>' + transport[i].engineCC + '</h6>'
     + '<h6 class="w-100"><b> Transmission: </b>' + transport[i].transmission + '</h6>'
+    + '<h6 class="w-100"><b> Cost: $ </b>' + cost + '</h6>'
+    + '<h6 class="w-100"><b> Distance: </b>' + distance + 'KM</h6>'
 
     + '<br><h6 class="w-100 text-justify"><b> Description: </b>' + transport[i].description + '</h6>'
     + '<br><h6 class="w-100 text-justify"><b> Features: </b>' + transport[i].features + '</h6>'
+    + '<div class="map"></div>'
     + '</div>'
     + '</div>'
     }
   }
+  //initMap()
 });
-
-//Show map
-$('#map').hide();
-$('#showMap').on('click', function(){
-  $('#map').show();
-});
+}
 
 
+
+//
 //vehicles
 document.getElementById('vehicles').addEventListener('click', function(){
   document.getElementById('show').innerHTML = " "; //to clear the container
@@ -339,21 +379,24 @@ document.getElementById('vehicles').addEventListener('click', function(){
       + '</div>'
       + '</div>';
   }
+  modals();
 });
 
   // Trigger Search Button
-
+    var days, people;
     $('.searchBtn').on('click', function(){
       // var days = spinner.spinner( "value" ) ;
       //Read the place values
       // x = parseInt(document.getElementById('nod').value);
-      var days = parseInt(document.getElementById('days').value);
+      days = parseInt(document.getElementById('days').value);
       if ((days > 15) || (days < 1)){
         alert('Please Enter between 1 to 15');
       }
-      var people = parseInt(document.getElementById('nop').value);
+       people = parseInt(document.getElementById('nop').value);
       console.log(days);
       console.log(people);
+      console.log(distance);
+
       document.getElementById('show').innerHTML = " ";
       for(var i = 0; i < transport.length; i++) {
      //   console.log('search result');
@@ -361,7 +404,7 @@ document.getElementById('vehicles').addEventListener('click', function(){
       if ((days >= transport[i].minDay) && (days<= transport[i].maxDay) &&(people >=transport[i].minPpl) && (people <= transport[i].maxPpl)){
       console.log('true');
       document.getElementById('show').innerHTML
-      += '<div class="card vCard myContainer border border-light clearfix py-0 ml-4 center mb-2 col-sm-6 col-md-6 col-lg-4">'
+      += '<br><div class="card vCard myContainer border border-light clearfix py-0 ml-4 center mb-2 col-sm-6 col-md-6 col-lg-4">'
       +  '<img src="' + transport[i].photo + ' " class="card-img-top myTransport" alt="Vehicles"/>'
       +  '<ul class="card-body py-1 px-1 list-group list-group-flush">'
       + '<li class="text-primary cardContent list-group-item">' + transport[i].vehicleName + '</li>'
@@ -372,4 +415,5 @@ document.getElementById('vehicles').addEventListener('click', function(){
       +  '</div>'
       }
     }
-});   // Vehicle Search ends here.
+    modals();
+});
